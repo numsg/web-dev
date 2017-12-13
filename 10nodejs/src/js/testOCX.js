73b8516fd775754af402f65480a7776b77d18a62
gaoqiang <gaoqiang@gsafety.com>
2017-12-13 15:55:25 +0800

    add: nodejs相关资料

--- 10nodejs/src/js/testOCX.js
+++ 10nodejs/src/js/testOCX.js
@@ -2,11 +2,11 @@ require('winax');
 var testOCX = new ActiveXObject("TESTOCX.TestOCXCtrl.1");
 var result = 'ok';
 try{
-	console.dir(testOCX);
-	var result = testOCX.test(1,2);
-	console.log(result);
+  console.dir(testOCX);
+  var result = testOCX.test(1,2);
+  console.log(result);
 } catch(ex){
-    result = ex.message;
-	console.log(result);
-	console.log(ex.stack);
+  result = ex.message;
+  console.log(result);
+  console.log(ex.stack);
 }
